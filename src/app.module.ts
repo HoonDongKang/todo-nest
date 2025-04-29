303fa9aa51fbf34f49d8a2ee2dcb4e985c7bf0df
HoonDongKang <d159123@naver.com>
2025-04-29 16:28:06 +0900

    feat: Add sqlite config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ConfigModule.forRoot(),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

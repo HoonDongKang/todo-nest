64461ba89123564508fbb3382776edc252b7d8ea
HoonDongKang <d159123@naver.com>
2025-04-30 16:05:19 +0900

    feat: Add visualize dependency graph

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SpelunkerModule } from 'nestjs-spelunker';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const tree = SpelunkerModule.explore(app);
+  const root = SpelunkerModule.graph(tree);
+  const edges = SpelunkerModule.findGraphEdges(root);
+  console.log('graph LR');
+  const mermaidEdges = edges.map(
+    ({ from, to }) => `  ${from.module.name}-->${to.module.name}`,
+  );
+  console.log(mermaidEdges.join('\n'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

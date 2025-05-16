5fbb1173871ce6edaee59f38217a419c215c7bf2
HoonDongKang <d159123@naver.com>
2025-05-16 15:34:17 +0900

    feat: Add auth domain

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/database/user.entity';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async register(createUserDto: CreateUserDto): Promise<User | null> {
+    return await this.userRepository.save(createUserDto);
+  }
+}

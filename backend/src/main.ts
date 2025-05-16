83ed6910d3dce235cbbdff7dafa21f5ef1d95c12
HoonDongKang <d159123@naver.com>
2025-05-16 15:45:59 +0900

    feat: Add enable CORS, set global prefix 'api'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
   // const tree = SpelunkerModule.explore(app);

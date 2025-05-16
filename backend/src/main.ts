44cd29fe9c165fa521d5c024f73b3e02c02400fe
HoonDongKang <d159123@naver.com>
2025-05-16 15:36:49 +0900

    chore: annotation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,15 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  const tree = SpelunkerModule.explore(app);
-  const root = SpelunkerModule.graph(tree);
-  const edges = SpelunkerModule.findGraphEdges(root);
-  console.log(tree);
-  console.log('graph LR');
-  const mermaidEdges = edges.map(
-    ({ from, to }) => `  ${from.module.name}-->${to.module.name}`,
-  );
-  console.log(mermaidEdges.join('\n'));
+  // const tree = SpelunkerModule.explore(app);
+  // const root = SpelunkerModule.graph(tree);
+  // const edges = SpelunkerModule.findGraphEdges(root);
+  // console.log(tree);
+  // console.log('graph LR');
+  // const mermaidEdges = edges.map(
+  //   ({ from, to }) => `  ${from.module.name}-->${to.module.name}`,
+  // );
+  // console.log(mermaidEdges.join('\n'));
 
   await app.listen(process.env.PORT ?? 3000);
 }
